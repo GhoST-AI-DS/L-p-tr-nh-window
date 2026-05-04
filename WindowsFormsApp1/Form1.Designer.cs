708cee24975c3142474735461cc4fa42c307741e
Nguyễn Văn Phong <fakergaming36@gmail.com>
2026-05-04 09:46:31 +0700

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.thayĐổiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.thayĐổiMàuSắcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.thayĐổiFontChữToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.txtBieuThuc = new System.Windows.Forms.TextBox();
+			this.txtKetQua = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.button13 = new System.Windows.Forms.Button();
+			this.button14 = new System.Windows.Forms.Button();
+			this.button15 = new System.Windows.Forms.Button();
+			this.button16 = new System.Windows.Forms.Button();
+			this.menuStrip1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thayĐổiToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(468, 24);
+			this.menuStrip1.TabIndex = 6;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// thayĐổiToolStripMenuItem
+			// 
+			this.thayĐổiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thayĐổiMàuSắcToolStripMenuItem,
+            this.thayĐổiFontChữToolStripMenuItem});
+			this.thayĐổiToolStripMenuItem.Name = "thayĐổiToolStripMenuItem";
+			this.thayĐổiToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+			this.thayĐổiToolStripMenuItem.Text = "Thay Đổi";
+			// 
+			// thayĐổiMàuSắcToolStripMenuItem
+			// 
+			this.thayĐổiMàuSắcToolStripMenuItem.Name = "thayĐổiMàuSắcToolStripMenuItem";
+			this.thayĐổiMàuSắcToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.thayĐổiMàuSắcToolStripMenuItem.Text = "Thay đổi màu sắc";
+			this.thayĐổiMàuSắcToolStripMenuItem.Click += new System.EventHandler(this.thayĐổiMàuSắcToolStripMenuItem_Click);
+			// 
+			// thayĐổiFontChữToolStripMenuItem
+			// 
+			this.thayĐổiFontChữToolStripMenuItem.Name = "thayĐổiFontChữToolStripMenuItem";
+			this.thayĐổiFontChữToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.thayĐổiFontChữToolStripMenuItem.Text = "Thay đổi font chữ";
+			this.thayĐổiFontChữToolStripMenuItem.Click += new System.EventHandler(this.thayĐổiFontChữToolStripMenuItem_Click);
+			// 
+			// txtBieuThuc
+			// 
+			this.txtBieuThuc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBieuThuc.BackColor = System.Drawing.Color.Gold;
+			this.txtBieuThuc.Location = new System.Drawing.Point(0, 28);
+			this.txtBieuThuc.Name = "txtBieuThuc";
+			this.txtBieuThuc.Size = new System.Drawing.Size(463, 20);
+			this.txtBieuThuc.TabIndex = 8;
+			// 
+			// txtKetQua
+			// 
+			this.txtKetQua.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtKetQua.BackColor = System.Drawing.Color.Aqua;
+			this.txtKetQua.Location = new System.Drawing.Point(0, 54);
+			this.txtKetQua.Name = "txtKetQua";
+			this.txtKetQua.Size = new System.Drawing.Size(463, 20);
+			this.txtKetQua.TabIndex = 8;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 4;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button4, 3, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button5, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.button6, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.button7, 2, 1);
+			this.tableLayoutPanel1.Controls.Add(this.button8, 3, 1);
+			this.tableLayoutPanel1.Controls.Add(this.button9, 3, 2);
+			this.tableLayoutPanel1.Controls.Add(this.button10, 2, 2);
+			this.tableLayoutPanel1.Controls.Add(this.button11, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.button12, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.button13, 0, 3);
+			this.tableLayoutPanel1.Controls.Add(this.button14, 1, 3);
+			this.tableLayoutPanel1.Controls.Add(this.button15, 2, 3);
+			this.tableLayoutPanel1.Controls.Add(this.button16, 3, 3);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 80);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 4;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(463, 223);
+			this.tableLayoutPanel1.TabIndex = 9;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(109, 49);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(118, 3);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(109, 49);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(233, 3);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(109, 49);
+			this.button3.TabIndex = 0;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(348, 3);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(109, 49);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "+";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(3, 58);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(109, 49);
+			this.button5.TabIndex = 0;
+			this.button5.Text = "4";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(118, 58);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(109, 49);
+			this.button6.TabIndex = 0;
+			this.button6.Text = "5";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(233, 58);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(109, 49);
+			this.button7.TabIndex = 0;
+			this.button7.Text = "6";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(348, 58);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(109, 49);
+			this.button8.TabIndex = 0;
+			this.button8.Text = "-";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(348, 113);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(109, 49);
+			this.button9.TabIndex = 0;
+			this.button9.Text = "*";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(233, 113);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(109, 49);
+			this.button10.TabIndex = 0;
+			this.button10.Text = "9";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(118, 113);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(109, 49);
+			this.button11.TabIndex = 0;
+			this.button11.Text = "8";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button12
+			// 
+			this.button12.Location = new System.Drawing.Point(3, 113);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(109, 49);
+			this.button12.TabIndex = 0;
+			this.button12.Text = "7";
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button13
+			// 
+			this.button13.Location = new System.Drawing.Point(3, 168);
+			this.button13.Name = "button13";
+			this.button13.Size = new System.Drawing.Size(109, 49);
+			this.button13.TabIndex = 0;
+			this.button13.Text = "C";
+			this.button13.UseVisualStyleBackColor = true;
+			this.button13.Click += new System.EventHandler(this.btnXoa_Click);
+			// 
+			// button14
+			// 
+			this.button14.Location = new System.Drawing.Point(118, 168);
+			this.button14.Name = "button14";
+			this.button14.Size = new System.Drawing.Size(109, 49);
+			this.button14.TabIndex = 0;
+			this.button14.Text = "0";
+			this.button14.UseVisualStyleBackColor = true;
+			this.button14.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// button15
+			// 
+			this.button15.Location = new System.Drawing.Point(233, 168);
+			this.button15.Name = "button15";
+			this.button15.Size = new System.Drawing.Size(109, 49);
+			this.button15.TabIndex = 0;
+			this.button15.Text = "=";
+			this.button15.UseVisualStyleBackColor = true;
+			this.button15.Click += new System.EventHandler(this.btnBang_Click);
+			// 
+			// button16
+			// 
+			this.button16.Location = new System.Drawing.Point(348, 168);
+			this.button16.Name = "button16";
+			this.button16.Size = new System.Drawing.Size(109, 49);
+			this.button16.TabIndex = 0;
+			this.button16.Text = "/";
+			this.button16.UseVisualStyleBackColor = true;
+			this.button16.Click += new System.EventHandler(this.btnNhap_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(468, 307);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.txtKetQua);
+			this.Controls.Add(this.txtBieuThuc);
+			this.Controls.Add(this.menuStrip1);
+			this.Name = "Form1";
+			this.Text = "Máy tính của Nguyễn Văn Phong";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem thayĐổiToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem thayĐổiMàuSắcToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem thayĐổiFontChữToolStripMenuItem;
+		private System.Windows.Forms.TextBox txtBieuThuc;
+		private System.Windows.Forms.TextBox txtKetQua;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.Button button13;
+		private System.Windows.Forms.Button button14;
+		private System.Windows.Forms.Button button15;
+		private System.Windows.Forms.Button button16;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data; 
+
+namespace WindowsFormsApp1
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            this.Text = "Máy tính của: Nguyễn Văn Phong";
+        }
+
+        
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            txtBieuThuc.Text += btn.Text;
+        }
+
+        
+        private void btnBang_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                var result = new DataTable().Compute(txtBieuThuc.Text, null);
+                txtKetQua.Text = result.ToString();
+            }
+            catch
+            {
+                txtKetQua.Text = "Lỗi biểu thức";
+            }
+        }
+
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            txtBieuThuc.Text = "";
+            txtKetQua.Text = "";
+        }
+
+   
+        private void thayĐổiMàuSắcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+           
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+               
+                foreach (Control control in tableLayoutPanel1.Controls)
+                {
+                    if (control is Button)
+                    {
+                        control.BackColor = colorDialog.Color;
+                    }
+                }
+            }
+        }
+
+        private void thayĐổiFontChữToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+
+            
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                this.Font = fontDialog.Font;
+
+
+                txtBieuThuc.Font = fontDialog.Font;
+                txtKetQua.Font = fontDialog.Font;
+
+                foreach (Control control in tableLayoutPanel1.Controls)
+                {
+                    if (control is Button)
+                    {
+                        control.Font = fontDialog.Font;
+                    }
+                }
+            }
+        }
+    }
+}
